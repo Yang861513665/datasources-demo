394ab6f79928fb049ff829487f06ff65a2f98783
Yang861513665 <861513665@qq.com>
2019-08-23 09:36:31 +0800

    Create TestEventBus.java
    使用  eventBus demo

---
+++ src/test/java/TestEventBus.java
@@ -0,0 +1,38 @@
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
+import org.junit.Test;
+
+
+public class TesEventBus {
+
+    @Test
+    public void testReceiveEvent() throws Exception {
+
+        EventBus eventBus = new EventBus("test");
+        EventListener listener = new EventListener();
+
+        eventBus.register(listener);
+
+        eventBus.post(new TestEvent(200));
+        eventBus.post(new TestEvent(300));
+        eventBus.post(new TestEvent(400));
+
+}
+
+}
+class EventListener{
+    @Subscribe
+    public void test(TestEvent testEvent){
+        System.out.println("eventListener: " + testEvent.getMessage());
+    }
+}
+class TestEvent {
+    private final int message;
+    public TestEvent(int message) {
+        this.message = message;
+        System.out.println("event: " + message);
+    }
+    public int getMessage() {
+        return message;
+    }
+}
